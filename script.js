953ec08b3f99c65ebf21226200205217e1f8bf03
zDeep10 <gabriel.wti02@gmail.com>
2021-03-13 18:44:56 -0300

    alterações

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector('.click').addEventListener('click', aparecer)
+document.querySelector('.click2').addEventListener('click', aparecer2)
+document.querySelector('.click3').addEventListener('click', aparecer3)
+
+function aparecer() {
+    document.querySelector('div.fadein1').style.opacity = '1'
+}
+
+function aparecer2() {
+    document.querySelector('div.fadein2').style.opacity = '1'
+}
+
+function aparecer3() {
+    document.querySelector('div.fadein3').style.opacity = '1'
+}
