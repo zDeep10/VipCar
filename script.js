b50b666a24130c540a5f95a1a8ade4c995c8cdc2
zDeep10 <gabriel.wti02@gmail.com>
2021-03-16 21:23:51 -0300

    corrigindo bugs

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-document.querySelector('.click').addEventListener('click', aparecer)
-document.querySelector('.click2').addEventListener('click', aparecer2)
-document.querySelector('.click3').addEventListener('click', aparecer3)
+// document.querySelector('.click').addEventListener('click', aparecer)
 
-function aparecer() {
-    document.querySelector('div.fadein1').style.opacity = '1'
+// document.querySelector('.click3').addEventListener('click', aparecer3)
+
+// function aparecer() {
+//     document.querySelector('div.fadein1').style.opacity = '1'
+// }
+
+function aparecer1() {
+    document.querySelector('.click1').classList.toggle("change")
 }
 
 function aparecer2() {
-    document.querySelector('div.fadein2').style.opacity = '1'
+    document.querySelector('.click2').classList.toggle("change")
 }
 
 function aparecer3() {
-    document.querySelector('div.fadein3').style.opacity = '1'
+    document.querySelector('.click3').classList.toggle("change")
 }
+
+// function aparecer3() {
+//     document.querySelector('div.fadein3').style.opacity = '1'
+// }
+
 
 
 
