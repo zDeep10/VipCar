7443fe340c5190a0934aeecd8f8e082e9a5e4492
zDeep10 <gabriel.wti02@gmail.com>
2021-03-19 15:15:19 -0300

    Adicionando pagina de cadastro

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
-// document.querySelector('.click').addEventListener('click', aparecer)
 
-// document.querySelector('.click3').addEventListener('click', aparecer3)
-
-// function aparecer() {
-//     document.querySelector('div.fadein1').style.opacity = '1'
-// }
 
 function aparecer1() {
-    document.querySelector('.click1').classList.toggle("change")
+    document.querySelector('.click1').classList.toggle("show")
 }
 
 function aparecer2() {
-    document.querySelector('.click2').classList.toggle("change")
+    document.querySelector('.click2').classList.toggle("show")
 }
 
 function aparecer3() {
-    document.querySelector('.click3').classList.toggle("change")
+    document.querySelector('.click3').classList.toggle("show")
 }
-
-// function aparecer3() {
-//     document.querySelector('div.fadein3').style.opacity = '1'
-// }
-
-
 
 
 function change() {
     document.querySelector('.bg-lista').classList.toggle("change")
     document.querySelector('.lista').classList.toggle("change")
+}
+
+// CADASTRO E LOGIN
+function show () {
+    document.querySelector('.formulario').classList.add("display")
+    document.querySelector('.formulario2').classList.remove("display")
+}
+
+function show2 () {
+    document.querySelector('.formulario2').classList.add("display")
+    document.querySelector('.formulario').classList.remove("display")
+}
+
+// X
+function sair () {
+    document.querySelector('.formulario').classList.remove("display")
+}
+
+function sair2 () {
+    document.querySelector('.formulario2').classList.remove("display")
+}
+
+
+function load () {
+    document.querySelector('.formulario').classList.add("display")
 }
